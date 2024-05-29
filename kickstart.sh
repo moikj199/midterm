6200ec116c853421a784f0b79899d6e6c7c6199f
moikj199 <156144336+moikj199@users.noreply.github.com>
2024-05-28 23:13:57 -0700

    Update kickstart.sh

--- kickstart.sh
+++ kickstart.sh
@@ -13,3 +13,6 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo mkdir /home/midterm
+cd /home/midterm
+sudo wget https://midterm2024cs.s3.amazonaws.com/docker-compose.yml
